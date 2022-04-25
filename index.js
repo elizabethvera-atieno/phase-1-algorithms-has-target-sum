19ee8261ee42cbdf3553d8e2668802ee7bf0b338
elizabethvera-atieno <atienolizvera@gmail.com>
2022-04-25 12:41:58 +0300

    complete test

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
 function hasTargetSum(array, target) {
-  // for (let i=0;i<array.lenghth;i++){  
-  //     const complement = target - array[i]
-  //     for(let j=0;j<array.lenghth;j++){
-  //       if(complement === array[j])return true
-  //     }  
-  // }
-  // return false
-
-  const seenNumbers = {};
-
-  for (const number of array) {
-    // n steps
-    const complement = target - number;
-    if (complement in seenNumbers) return true;
-    seenNumbers[number] = true;
-  }
-
+   for (let i = 0; i < array.length; i++) {
+    const complement = target - array[i];
+    for (let j = i + 1; j < array.length; j++) {
+      if (array[j] === complement) return true;
+    }
+   }
   return false;
+
+//   const seenNumbers = {};
+
+//   for (const number of array) {
+//     // n steps
+//     const complement = target - number;
+//     if (complement in seenNumbers) return true;
+//     seenNumbers[number] = true;
+//   }
+
+//   return false;
 }
 
 /* 
